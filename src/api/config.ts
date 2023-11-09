4d05b2dc6a7f0900afbd50131b2a450a8a109d3d
Divyansh Gupta <divyanshg809@gmail.com>
2023-11-09 12:08:22 +0530

    api change

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,5 +1,5 @@
-// const API_URL = "https://staging.astrokleem.in/api/v1";
-const API_URL = "http://localhost:8000/api/v1";
+const API_URL = "https://staging.astrokleem.in/api/v1";
+// const API_URL = "http://localhost:8000/api/v1";
 const SOCKET_URL = "http://localhost:4000";
 
 export default {
